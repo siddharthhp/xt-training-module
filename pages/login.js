fc4f9f43c0978d0b29d3d8a02d4c07e9e4495a7d
Siddharth Kota <ksiddharth1@sapient.com>
2018-05-14 12:49:05 +0530

    Logic added for login page showing up for already logged in users

--- pages/login.js
+++ pages/login.js
@@ -1,48 +1,38 @@
-import Helmet from "react-helmet";
-import Layout from "../components/Layout.js";
-import React, { Component } from "react";
-import fetch from "isomorphic-unfetch";
-import Link from "next/link";
-import Auth from "../components/Auth.js";
-import Login from "../components/Login.js";
+import Helmet from 'react-helmet';
+import Layout from '../components/Layout.js';
+import React, { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
+import Auth from '../components/Auth.js';
+import Login from '../components/Login.js';
 
-class LoginPage extends Component {   
-	constructor(props) {
-        super(props);
-        this.state={showLoginPage: false};
-    }
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    authValidation = async (event) => {
-        const key = sessionStorage.getItem('AuthToken');
-        if (key) {
-            this.setState({showLoginPage: true});
-        }
+  authValidation = async event => {
+    const key = sessionStorage.getItem('AuthToken');
+    if (key) {
+      window.location.href = './';
     }
+  };
 
-    componentWillMount() {
-        this.authValidation();
-    }
+  componentWillMount() {
+    this.authValidation();
+  }
 
-    render() {
-    	if(this.state.showLoginPage) {
-	        return (
-	        	<div className = "login-container">
-		        	Already logged in !
-		        </div>
-	        );
-    	} else {
-    		return (
-	        	<div className = "login-container">
-		        	<Helmet>
-				        <link rel="stylesheet" href="/static/styles/style.css" />
-				    </Helmet>
-				    <h1> Login Form</h1>
-		            <Login>                
-		            </Login>
-		        </div>
-	        );
-    	}
-    }
+  render() {
+    return (
+      <div className="login-container">
+        <Helmet>
+          <link rel="stylesheet" href="/static/styles/style.css" />
+        </Helmet>
+        <h1> Login Form</h1>
+        <Login />
+      </div>
+    );
+  }
 }
 
 export default LoginPage;
